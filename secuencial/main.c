b8267b65a807109a18125b1c5aa543886a0d4abe
Aldo Jurado <emilio.jurado@ciencias.unam.mx>
2026-06-09 11:58:27 -0600

    versión secuencial

--- secuencial/main.c
+++ secuencial/main.c
@@ -1,31 +1,25 @@
-#include <mpi.h>
 #include <string.h>
 #include "structure.h"
 /**
  * Main principal que ejecuta el programa
- * 
+ *
  * Se le pasa el numero de procesos
  * Cada proceso ejecuta el main (dependiendo el rank):
  * - rank= 0: es el master
  * - rank /= 0 es worker
  */
-int main(int argc, char **argv) {
-    MPI_Init(&argc,&argv); // Iniciar MPI
-    // Obtener rank (ID)
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+int main(int argc, char **argv)
+{
+
     // Crear configuracion (archivo de entrada, carpeta de trabajo (.pts y .tri))
     CONFIG cfg;
-    strcpy(cfg.input_file,"data/prueba_100.las");
-    strcpy(cfg.work_dir,"work");
-    strcpy(cfg.svg_out,    argc > 1 ? argv[1] : "resultado.svg");
+    strcpy(cfg.input_file, "data/prueba_100.las");
+    strcpy(cfg.work_dir, "work");
+    strcpy(cfg.svg_out, argc > 1 ? argv[1] : "resultado.svg");
     // Definir tamaño del bloque
     cfg.block_size = 20000;
-    // Dependiendo del rank ejecutara su rol
-    if(rank==0)
-        master_run(&cfg);
-    else
-        worker_loop(rank);
-    MPI_Finalize(); // Finalizar proceso paralelizable
+
+    // Ejecución secuencial
+    sequential_run(&cfg);
     return 0;
 }
--- secuencial/structure.h
+++ secuencial/structure.h
@@ -1,43 +1,45 @@
 #ifndef STRUCTURE_H
 #define STRUCTURE_H
-#define TAG_TASK 1 // Etiqueta de mensaje cuando master le envía mensaje a un worker
-#define TAG_DONE 2 // Etiqueta de mensaje cuando worker termina tarea
-#define TAG_STOP 3 // Etiqueta de mensaje cuando el master envía señal de detener la ejecución o que ha terminado
+
 /*
  * Definiciones que son compartidas en varios archivos
  */
 
 /* Definición de un punto (x,y,z) */
-typedef struct {
+typedef struct
+{
     double x, y, z; // Coordenadas .las (nube de puntos)
 } POINT;
 
 /* Definición de triángulo (para la triangulación) */
-typedef struct {
+typedef struct
+{
     double x0, y0; // Vertice A
     double x1, y1; // Vertice B
     double x2, y2; // Vertice C
 } TRIANGLE;
 
 /* Bloque (grid) */
-typedef struct {
-    int id; // Identificador
+typedef struct
+{
+    int id;             // Identificador
     char pts_file[256]; // Archivo con puntos de ese bloque (.pts)
     char tri_file[256]; // Archivo donde se guardan (.tri)
 } BLOCK;
 
 /* Parte de la configuracion */
-typedef struct {
+typedef struct
+{
     char input_file[256]; // Archivo .las
-    char work_dir[256]; // Archivo donde se guarda (.pts y .tri)
+    char work_dir[256];   // Archivo donde se guarda (.pts y .tri)
     char svg_out[256];
     int block_size; // // Tamaño del bloque
 } CONFIG;
 
 /**
  * Función para leer los puntos del .las
- * 
- * Args: 
+ *
+ * Args:
  * - file: archivo .las
  * - pts: arreglo de puntos (apunta directo a un dato Point)
  * - n: cantidadad
@@ -46,7 +48,7 @@ int las_read_points(const char *file, POINT **pts, int *n);
 
 /**
  * Divide los puntos de un bloque
- * 
+ *
  * Args:
  * - cfg: configuración (.las, salida, tamaño bloque, tamaño bloque)
  * - blocks: divide los puntos en un arreglo de bloques
@@ -57,7 +59,7 @@ int split_run(const CONFIG *cfg, BLOCK **blocks, int *num_blocks);
 /**
  * Uner los resultados por niveles
  */
-int  merge_all(BLOCK *blocks, int nb, char *out_path); 
+int merge_all(BLOCK *blocks, int nb, char *out_path);
 
 /**
  * Une los resultados de la triangulación
@@ -66,7 +68,7 @@ int merge_run(const CONFIG *cfg, BLOCK *blocks, int nb);
 
 /**
  * Aplica la trinagulación de Delaunay
- * 
+ *
  * Args:
  * - pts: puntos
  * - n: n triaungulos
@@ -77,15 +79,10 @@ int delaunay_run(POINT *pts, int n, const char *out_file);
 void write_svg(const char *svg_path, POINT *pts, int n, const char *tri_bin);
 
 /**
- * Funcion de cada worker
- */
-void worker_loop(int rank);
-
-/**
- * Funcion del master
- * 
+ * Funcion principal
+ *
  * Nota: recibe cfg porque es el que tomará el archivo .las y lo va a dividir en bloques
  */
-int master_run(const CONFIG *cfg);
+int sequential_run(const CONFIG *cfg);
 
 #endif
